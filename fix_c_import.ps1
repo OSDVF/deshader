327d5c44ef686fed05183200fce943f8b3c4bd49
OSDVF <o.s.dv.f@seznam.cz>
2024-05-22 19:15:35 +0200

    Fix build

--- fix_c_import.ps1
+++ fix_c_import.ps1
@@ -1,9 +1,8 @@
-# if ZIG_GLOBAL_CACHE_DIR is not set, set it to zig-cache
-if (-not $env:ZIG_GLOBAL_CACHE_DIR) {
-    $env:ZIG_GLOBAL_CACHE_DIR = "zig-cache"
+if (-not $env:ZIG_LOCAL_CACHE_DIR) {
+    $env:ZIG_LOCAL_CACHE_DIR = "zig-cache"
 }
 
-Get-ChildItem -Path $env:ZIG_GLOBAL_CACHE_DIR -Recurse -Filter cimport.zig | ForEach-Object {
+Get-ChildItem -Path $env:ZIG_LOCAL_CACHE_DIR -Recurse -Filter cimport.zig | ForEach-Object {
     $file = $_.FullName
     Write-Host "Fixing $file"
     Set-Content -Path $file -Value (get-content -Path $file | Select-String -Pattern 'struct_XSTAT' -NotMatch)
--- fix_c_import.sh
+++ fix_c_import.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-if [ -z "$ZIG_GLOBAL_CACHE_DIR" ]; then
-    ZIG_GLOBAL_CACHE_DIR="zig-cache"
+if [ -z "$ZIG_LOCAL_CACHE_DIR" ]; then
+    ZIG_LOCAL_CACHE_DIR="zig-cache"
 fi
 
-for file in ~/.cache/zls/*/*/cimport.zig $ZIG_GLOBAL_CACHE_DIR/*/*/cimport.zig
+for file in ~/.cache/zls/*/*/cimport.zig $ZIG_LOCAL_CACHE_DIR/*/*/cimport.zig
 do
     if [ ! -f "$file" ]; then
         continue
